78b57b1b838623c8b4e78a2ec5890bdc80f15231
RuanJH <1018662023@qq.com>
2025-04-06 18:24:27 +0800

    feat(core):初步完成核心逻辑、调整代码结构

---
+++ Backend/Core/SubtitleFile/SubtitleParserFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace SubtitleTranslator.Backend.FileParser;
+
+public class SubtitleParserFactory
+{
+    public static ISubtitleFileParser CreateInstance(string fileName)
+    {
+        //获取文件名的后缀
+        var fileSuffix = Path.GetExtension(fileName);
+        switch (fileSuffix)
+        {
+            case ".vtt":
+                return new VttParser();
+            default:
+                //抛出异常
+                throw new FileFormatException("文件格式不支持");
+        }
+    }
+}
---
+++ Backend/Core/SubtitleTranslator.cs
@@ -0,0 +1,24 @@
+﻿using SubtitleTranslator.Backend.Obj;
+using SubtitleTranslator.Backend.TranslateService;
+
+namespace SubtitleTranslator.Backend.FileParser;
+
+public class SubtitleTranslator
+{
+    public async Task TranslateFile(string filePath)
+    {
+        var directoryPath = Path.GetDirectoryName(filePath);//文件所在目录
+        var fileName = Path.GetFileName(filePath);//文件名
+        var newFileName = Path.GetFileNameWithoutExtension(fileName) + "_translated" + Path.GetExtension(fileName);//新文件名
+        var fileParser = SubtitleParserFactory.CreateInstance(fileName);
+        var baiduTranslateService = ITranslateService.GetService(ITranslateService.ServiceType.BaiduApi);
+        var subtitleItems = fileParser.Read(filePath);
+        var enumerable = subtitleItems as SubtitleItem[] ?? subtitleItems.ToArray();
+        foreach (var subtitleItem in enumerable)
+        {
+            var translatedTex = await baiduTranslateService.TranslateAsync(subtitleItem.Text);
+            subtitleItem.TranslatedText = translatedTex;
+        }
+        await fileParser.WriteAsync(newFileName, enumerable);
+    }
+}
